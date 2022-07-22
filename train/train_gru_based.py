0b675f532d297d89efda904a850b08c37172155f
welch <welch_cn@163.com>
2022-07-23 00:31:05 +0800

    模型设计

--- train/train_gru_based.py
+++ train/train_gru_based.py
@@ -28,6 +28,7 @@ LR = float(config.get(section, "LR"))
 STEP = int(config.get(section, "STEP"))
 CHARGE_RADIUS = int(config.get(section, "CHARGE_RADIUS"))
 PENALTY_RADIUS = int(config.get(section, "PENALTY_RADIUS"))
+LAMDA = float(config.get(section, "LAMDA"))
 
 corpus_info_path = "../dataprepare/lang-CAIL-SMALL.pkl"
 data_path = "../dataset/CAIL-SMALL/train_processed.txt"
@@ -138,7 +139,7 @@ for step in range(STEP):
     penalty_labels = torch.cat(penalty_labels, dim=0).to(device)
     penalty_preds_loss = criterion(penalty_preds_outputs, penalty_labels)
 
-    loss = posi_pairs_dist+neg_pairs_dist+charge_preds_loss+article_preds_loss+penalty_preds_loss+0.1*penalty_contrains_loss
+    loss = posi_pairs_dist+neg_pairs_dist+charge_preds_loss+article_preds_loss+penalty_preds_loss+LAMDA*penalty_contrains_loss
     train_loss += loss.item()
 
     # 反向传播计算梯度
